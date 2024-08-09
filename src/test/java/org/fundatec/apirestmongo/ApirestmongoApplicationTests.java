013e30a713856464d1b574b1f5e228c455baefd8
arturbazza <agencia@fonzie.com.br>
2024-08-09 10:40:40 -0300

    Start projeto inicial

---
+++ src/test/java/org/fundatec/apirestmongo/ApirestmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fundatec.apirestmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
